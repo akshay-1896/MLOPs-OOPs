513d6e8c0d63d0a1b4262482080eb9043e011b2d
Akshay Sharma <officialakshay1896@gmail.com>
2025-07-15 16:07:34 +0530

    1 - class and obj basics

---
+++ oops1.py
@@ -0,0 +1,25 @@
+# initiate a class
+class employee:
+    # special method/ magic method/ dunder method - constructor
+    def __init__(self):
+        # print("Started initialising attributes!!")
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SDE"
+        # print("Attributes are initialized")
+        
+    def travel(self, destination):
+        print("This travel method was called manually")
+        # print(f"Employee with ID {self.id} is traveling to {destination}.")
+
+
+# create an object/instance of the class
+sam = employee()
+
+# print(sam.id)
+# print(sam.designation)
+# print(sam.salary)
+
+# sam.travel("Kerala")
+
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,18 @@
+lst = [1,2,3]
+my_str = "MLOPs Tutorial"
+my_int = 155
+
+# print(type(my_str))
+# print(type(my_int))
+# print(type(lst))
+
+# lst.clear()
+# lst.capitalize() # This will raise an AttributeError since lists do not have a capitalize method
+# print(lst)
+# my_str = my_str.capitalize()
+# print(my_str)
+
+# a = 'x'
+# b = 'y'
+
+# print(a + b)
