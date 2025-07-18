723dbe806e3dad691b94ec17580a0f9c5cab08cb
Akshay Sharma <officialakshay1896@gmail.com>
2025-07-18 13:53:11 +0530

    Encapsulation, Getter & Setter

--- rough.py
+++ rough.py
@@ -17,5 +17,46 @@ my_int = 155
 
 # print(a + b)
 
+
+# function
+# print(len(lst))
+
+# method
 from oops_project import chatbook
 user1 = chatbook()
+# user1.send_msg()
+# print(user1.name)
+# print(user1.__name)  # This will raise an AttributeError since __name is private
+# print(user1._chatbook__name)
+# print(user1.get_name())
+# print(user1.set_name("New User"))
+# print(user1.get_name())
+
+print(user1.id)
+
+chatbook.set_id(10)
+
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
